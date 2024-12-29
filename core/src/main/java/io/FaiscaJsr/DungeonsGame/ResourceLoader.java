1d9059529cb3d1a69817ac8655b11855e953564c
VioletGminisJ25 <javierrodal25@gmail.com>
2024-12-29 18:42:06 +0100

    class for load resources

--- core/src/main/java/io/FaiscaJsr/DungeonsGame/ResourceLoader.java
+++ core/src/main/java/io/FaiscaJsr/DungeonsGame/ResourceLoader.java
@@ -8,9 +8,25 @@ import io.FaiscaJsr.DungeonsGame.entities.Tile;
 
 public class ResourceLoader {
 	static Texture tiles = new Texture("tileset_complet.png");
-	public static Sprite groundTile(){
+	public static Sprite floorTile(){
 		int x = 32;
 		int y = 32;
 		return new Sprite(new TextureRegion(tiles,x,y,Tile.DIM*2,Tile.DIM*2));
+	}
+	public static Sprite wallTile(int rotation){
+		int x = 32;
+		int y = 0;
+		TextureRegion region = new TextureRegion(tiles,x,y,Tile.DIM*2,Tile.DIM*2);
+		Sprite sprite = new Sprite(region);
+		sprite.rotate(rotation);
+		return sprite;
+	}
+	public static Sprite cornerTile(int rotation){
+		int x = 0;
+		int y = 0;
+		TextureRegion region = new TextureRegion(tiles,x,y,Tile.DIM*2,Tile.DIM*2);
+		Sprite sprite = new Sprite(region);
+		sprite.rotate(rotation);
+		return sprite;
 	}
 }
